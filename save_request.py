0c5860de4f87b692ab28105bfe29ed3fddac379d
陈永园 <ocyy@foxmail.com>
2024-08-06 23:27:26 +0800

    目前刷课脚本 能够自动读取每个课程的进度，只刷不够的课程 增加慢刷选择，慢刷安全些 自动读取课程id 每个课程只刷所需要的时间，绝不多刷

--- save_request.py
+++ save_request.py
@@ -1,52 +1,62 @@
 import requests
-
-
-def get_all_class_id():
-    class_ids = [
-        "9be29932adca13a8761a810ad9ff1201",
-        "84826a077a3ee7133da28370999c4c4c",
-        "9ec7cb7aa59a07f22728a89ffbbcef36",
-        "c484e08190c7f908a961d774f1a2b5d3",
-        "0de584669b679c2206acfca06522c8c2",
-        "79a39e3ce9b4509a7fc1ca3f29c0b869",
-        "04b4067f100dce9281bcf7ada15fe658",
-        "478ff9d64a0628e3dc5622c26111bc7d",
-        "345ee2eee7189a6e3507c755b34e1316",
-        "35e292a1fb2f0e388c29fc1ac10f4c99",
-        "4ba2e4620b5b6f3f38c46f15ba0312f7",
-        "37735aaafc0be61faf07b892a22685bd",
-        "5eaddb36193b306c4b8d02dd1e802583",
-        "d3d144557cea570a37fcb283d5f688eb",
-        "35e292a1fb2f0e388c29fc1ac10f4c99",
-        "7dafbc08703db2aba98f6fa5991d6136",
-        "21a99f1d4ddfa2661f93d0c5e54c2abb",
-        "5b1416709697175da4a2015a31b1fc9c",
-        "f07e6ee8c6ef732a212f6d55974a3daf",
-    ]
-    return class_ids
-
-
-# TODO: 请将下面的Cookie信息替换为你自己的Cookie信息
+import time
+from time import sleep
+import json
+import os
+import sys
+import sys
+import time
+#看自己浏览器里面的cookies，如果有没有的字段，需要手动加上,每个字段都要写，某些某些视频无法刷
 cookies = {
-    "JSESSIONID": "input_your_JSESSIONID_here",
-    "_csrf": "input_your__csrf_here",
-    "_pv0": "input_your__pv0_here",
-    "_pf0": "input_your__pf0_here",
-    "_pc0": "input_your__pc0_here",
-    "iPlanetDirectoryPro": "input_your_iPlanetDirectoryPro_here",
-    "stu-token": "input_your_stu-token_here",
+    "JSESSIONID": "",
+    "_csrf": "",
+    "_pv0": "",
+    "_pf0": "",
+    "_pc0": "",
+    "_pm0":"",
+    "iPlanetDirectoryPro": "",
+    "stu-token": "",
 }
-
-if __name__ == "__main__":
-    if cookies["stu-token"] == "input_your_stu-token_here":
+cookies = {
+    "JSESSIONID": "CFF4A67BA67FF90664E358CA0510EEF7",
+    "_csrf": "S8mwplVi9KWoF2WQ0TlCeNKhcpPcCrki%2F2cCMHtF%2B7c%3D",
+    "_pv0": "635kHYG9rOi%2BBHMDMh5a3ge0wLF7O1ynje4tWtPxln9061MQ4XWy9WqY47oLL%2FLoGkyt%2F4mpqlZNZdFc4a60QsHAmpJRogA%2By1CQc22GucyebomEkxfp2klEEdYjQ1%2BFZnJFiicV%2F3rbz09w%2FKBZ3krzUo2G4XCXWBRWXGQaznmhQbwHGBDlVFK8wzteCXMkhyFlJN201HGHVcBf3bPuExNZoewcLqZDZUmkA0%2FQak18TDU6cjAk%2BDTvUn%2BHfXIuV91yGmJ%2BUXNT4lWxNxB60DwmpQWo7ko1g8t4rO0AX4PAejS63LbtqdRTv0NV9U074lgEqx6TtHSD0rvUK9gVhcGWlFaEYKgFktl8kUWsZT%2BFsEsyBGC3C6YX2zm1SgBkWGJbPOGUCMcbPsF7Ro%2BMPPvLdTWtbfjqorf8a6K%2FQEM%3D",
+    "_pf0": "KKL%2BQ7DiP3SCcpQrwmFzEgXLgaPaBx4zJDL0WMTo4rY%3D",
+    "_pc0": "ZF1AKfG8yOMoTRJS6fMbxorNsAaiDlQpwiWPi6bR30w%3D",
+    "_pm0":"5TfJ%2BQU%2FgbLUVgsjZmtXW2TnY6FG1Do7oz04YKnXkd4%3D",
+    "iPlanetDirectoryPro": "Z0aunJC8FRTz08zLcU3z9X4MZW%2Bgftdhhplq%2Bx5m3nY%2BbJrt9wlb2X%2FRWZO4T92rbPb1KSvVz7Jy%2BiwC32VEhRuRg1evNpK%2BSPYfMnmYeOz0x2pOmJyQxDBYf4x8Av8g1%2BmL5mkLsznOQ8tbHLhziG7rc23fPUEslhd%2BMd27hMPTEmaytMMQ9U5Orq0gPi5MJPRyCiTqmjPJ%2FsK2BXpVZh6ASpaZh6Bx4m7zD3SJlcSDMK%2FGM3jN5yPioDwU1TiJvdvoOgnLOP51DREAUf4o9TOnLDAWfAQRaOf9j9lQ5M%2FAzhA5UXaF71dle6T5hzZfdGF81IBEOl4%2FmvfVU4iu5J7dsxmPwqBK%2BPjGws3BDmk%3D",
+    "stu-token": '81d68095d86f92bd1cb7df5aaf015cd8'
+}
+for key,val in cookies.items():
+    if val=="":
         raise Exception("请先将Cookie信息替换为你自己的Cookie信息")
-    url = "http://regi.zju.edu.cn/grs-pro/stu/classinfo/addStudyLog"
-    params = {
-        "t": 1722586488197,
-        "studyTime": 500,
-        "token": cookies["stu-token"]
-    }
-    payload = "t=1722586488197&studyTime=500&token="+cookies["stu-token"]
+token=cookies['stu-token']
+option=input("秒刷输入1，慢刷(安全些)输入2\n")
+
+def progress_bar(duration, length=50):
+    """显示一个在指定时间内完成的进度条。
+
+    Args:
+    duration (int): 进度条完成所需的总时间（秒）。
+    length (int): 进度条的长度（单位：字符）。
+    """
+    for i in range(duration + 1):
+        percent = (i / duration) * 100  # 计算百分比
+        bar_length = int((i / duration) * length)  # 计算当前应有的进度条长度
+        bar = '█' * bar_length + '-' * (length - bar_length)  # 创建进度条图形
+        sys.stdout.write(f'\rProgress: |{bar}| {i}s/{duration}s Complete')
+        sys.stdout.flush()
+        time.sleep(1)  # 等待一秒
+
+    print('\nProgress complete!')
+
+def get_current_timestamp():
+    # 获取当前时间的时间戳，单位为毫秒
+    return int(time.time() * 1000)
+
+def getStuDetail(token):
+    timestamp = get_current_timestamp()
+    payload = f"t={timestamp}&token={token}"
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Accept-Encoding": "gzip, deflate",
@@ -55,15 +65,106 @@ if __name__ == "__main__":
         "Content-Length": "0",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
         "Origin": "http://regi.zju.edu.cn",
-        "token": cookies["stu-token"],
+        "token": token
     }
 
-    for class_id in get_all_class_id():
-        cur_payload = payload + "&classId=" + class_id
-        params["classId"] = class_id
-        response = requests.post(url, params=params, data=payload, headers=headers, cookies=cookies)
-        print("class_id:", class_id)
-        # 输出响应内容
-        print("状态码:", response.status_code)
-        print("响应内容:", response.text)
-        print("=====================================")
+    # 发送POST请求
+    response = requests.get('http://regi.zju.edu.cn/grs-pro/stu/vstudydetail/getStuDetail', data=payload, headers=headers,cookies=cookies)
+    # print(f" Status Code: {response.status_code}, Response: {response.text}")
+
+    stuDetail = json.loads(response.text)
+
+    return stuDetail
+def get_courses(base_url):
+    # 生成时间戳
+    timestamp = get_current_timestamp()
+    url = f"{base_url}?t={timestamp}&title=&isRequired="
+    # 发送GET请求获取课程信息
+    response = requests.get(url)
+    return response.json()
+    course_data = response.json()
+    course_ids = [course['id'] for course in course_data['list']]
+    timeLimits= [course['timeLimit'] for course in course_data['list']]
+
+    return course_ids,timeLimits
+
+
+def post_study_log(base_url, token, course_ids,timeLimits):
+    cnt=0
+    for course_id,timeLimit in zip(course_ids,timeLimits):
+        # 生成时间戳
+        timestamp = get_current_timestamp()
+        # 设置参数和payload
+        params = {
+            "t": timestamp,
+            "studyTime": timeLimit,
+            "classId": course_id,
+            "token": token
+        }
+        payload = f"t={timestamp}&studyTime=500&classId={course_id}&token={token}"
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+            "Content-Length": "0",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+            "Origin": "http://regi.zju.edu.cn",
+            "token": token
+        }
+
+        # 发送POST请求
+        response = requests.post(base_url, params=params, data=payload, headers=headers,cookies=cookies)
+        print(f"Course ID: {course_id}, Status Code: {response.status_code}, Response: {response.text}")
+        cnt+=1
+        # if cnt==5:
+        #     break
+        # sleep(int(timeLimit))
+
+flag=1
+while flag==1:
+    flag=0
+    # 获取课程ID
+    courses=get_courses("http://regi.zju.edu.cn/grs-pro/config/vclassesdetail/queryList")
+    print(courses['list'])
+    StuDetails=getStuDetail(token)
+    print(StuDetails['list'])
+
+    course_ids = [course['classId'] for course in StuDetails['list']]
+    StuDetails['list']+=[course for course in courses['list'] if course['id'] not in course_ids]
+    print(StuDetails['list'])
+    print(len(StuDetails['list']))
+    # exit()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("----------------------------------------------------------")
+    tot=1
+    for course in StuDetails['list']:
+
+        if 'studyTime' not in course:
+            course['studyTime']=0
+        if 'classId' not in course:
+            course['classId']=course['id']
+        course['studyTime']=int(course['studyTime'])
+        course['timeLimit']=int(course['timeLimit'])
+
+        if course['studyTime']>=course['timeLimit']:
+            print(""+f"{course['title']}({course['studyTime']}/{course['timeLimit']})"+"\033[32m已完成\033[0m")
+            tot+=1
+        else:
+            print(f"已完成课程数量\033[32m{tot}/{len(StuDetails['list'])}\033[0m")
+            flag=1
+            print(""+f"{course['title']}({course['studyTime']}/{course['timeLimit']})"+"\033[31m未完成\033[0m")
+            need=course['timeLimit']-course['studyTime']
+            print(f"需要{need}s")
+
+            post_study_log("http://regi.zju.edu.cn/grs-pro/stu/classinfo/addStudyLog",token, [course['classId']],[need])
+            if option=="2":
+                progress_bar(need)
+            progress_bar(3)
+            break
+
+
+print("----------------------------------------------------------")
+
+# 对每个ID发送POST请求记录学习日志
+# post_study_log("http://regi.zju.edu.cn/grs-pro/stu/classinfo/addStudyLog",token, course_ids,timeLimits)
